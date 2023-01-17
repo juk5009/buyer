ae16c743716529bb377cfa074733f770c08609b3
Kjuk <juk6340@gmail.com>
2023-01-17 16:17:59 +0900

    자바 모델링 완료

--- src/main/java/shop/mtcoding/buyer/model/User.java
+++ src/main/java/shop/mtcoding/buyer/model/User.java
@@ -1,5 +1,17 @@
 package shop.mtcoding.buyer.model;
 
+import java.security.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private Timestamp createdAt;
 
 }
